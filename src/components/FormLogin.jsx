3b5381a9c20af4d58aa718d46eb679f202cd2cee
Lokisley Oliveira <lokisley@hotmail.com>
2022-07-15 14:42:11 -0300

    feat: save form values and setup submit function

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button, Col } from "react-bootstrap";
 
 function FormLogin() {
+  const [values, setValues] = useState({});
+
+  const onFormChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
+    console.log(name, value);
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+    event.persist();
+    console.log("push data somewhere :)");
+    console.log(values);
+
+    //TODO: make login request to the back-end
+    //TODO: if login success, set cookie with user data
+    //TODO: if login fails, show error message
+  };
+
   return (
-    <Form>
+    <Form onSubmit={submit}>
       <Col sm={8} className="d-flex flex-column mx-auto form-panel">
         <h3 className="fw-bold mb-3">Fazer login</h3>
         <Form.Group className="mb-3">
           <Form.Label>Email, CPF ou PIS</Form.Label>
-          <Form.Control type="email" />
+          <Form.Control name="email" type="email" onChange={onFormChange} />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Senha</Form.Label>
-          <Form.Control type="password" />
+          <Form.Control
+            name="password"
+            type="password"
+            onChange={onFormChange}
+          />
         </Form.Group>
-        <Button variant="primary" type="submit" className="mt-3 ms-auto">
+        <Button onClick={submit} variant="primary" className="my-3 ms-auto">
           Entrar
         </Button>
       </Col>
